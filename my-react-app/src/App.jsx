dd7cd17629e17982850f685cab8742b3cd49c439
Protengstar <musthofasifaulq@gmail.com>
2024-02-28 20:54:09 +0700

    Setup project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center bg-blue-100 min-h-screen items-center">
+      <div>Hello Wolrd!</div>
+    </div>
+  )
+}
+
+export default App;
