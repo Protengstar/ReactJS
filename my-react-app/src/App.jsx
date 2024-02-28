232da92e6ec0343894adc3bd92bfae6956271918
Protengstar <musthofasifaulq@gmail.com>
2024-02-28 21:30:07 +0700

    Pengenalan Atomic design

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,21 +1,15 @@
 import React from "react"
-
-const Button= (props) => {
-  const {children ="...", variant ="bg-black"} = props
-  return (
-    <button className={`h-10 px-6 font-semibold rounded-md ${variant} text-white`} type="submit">{children}</button>
-);
-}
+import Button from "./components/Elements/Button";
 
 function App () {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div className="flex gap-x-3">
+      <div className="flex gap-x-2">
         <Button variant="bg-red-400" >login</Button>
-        <Button variant="bg-slate-700" >logout</Button>
+        <Button variant="bg-slate-700" >log out</Button>
         <Button></Button>
       </div>
-    </div>
+    </div>      
   );
 }
 
