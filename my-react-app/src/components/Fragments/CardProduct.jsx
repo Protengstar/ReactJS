fe75bca620bf52c8fb69873d07eb470d0f346ad2
Protengstar <musthofasifaulq@gmail.com>
2024-02-29 14:24:44 +0700

    update {

--- my-react-app/src/components/Fragments/CardProduct.jsx
+++ my-react-app/src/components/Fragments/CardProduct.jsx
@@ -3,7 +3,7 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
     const { children } = props;
     return (
-        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-3">
+        <div className="w-full max-w-sm bg-gray-800 border border-gray-700 rounded-lg shadow mx-3 flex flex-col justify-between">
             {children}
         </div>
     );
@@ -19,11 +19,11 @@ const Header = (props) => {
 };
 
 const Body = (props) => {
-    const { children, title } = props;
+    const { children, name } = props;
     return (
-        <div className="px-5 pb-5">
+        <div className="px-5 pb-5 h-full">
             <a href="">
-                <h3 className="text-xl font-semibold tracking-tight text-white">{title}</h3>
+                <h3 className="text-xl font-semibold tracking-tight text-white">{name}</h3>
                 <p className="text-s text-white">{children}</p>
             </a>
         </div>
--- my-react-app/src/pages/products.jsx
+++ my-react-app/src/pages/products.jsx
@@ -1,18 +1,39 @@
 import CardProduct from "../components/Fragments/CardProduct";
 
+const products = [
+    {
+        id: 1,
+        name: "Coffee",
+        price: "Rp 23.000,00",
+        image: "/img/1.jpg",
+        description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur nulla corrupti, vitae quae quidem iusto modi maiores! Eligendi nihil repudiandae odio debitis consequatur est ullam, odit placeat similique ducimus!`,
+    },
+    {
+        id: 2,
+        name: "Tea",
+        price: "Rp 21.000,00",
+        image: "/img/2.jpg",
+        description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur nulla corrupti, vitae quae quidem iusto modi maiores!`,
+    },
+    {
+        id: 3,
+        name: "jus",
+        price: "Rp 30.000,00",
+        image: "/img/3.jpg",
+        description:`Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores molestias saepe fugiat, dicta vero perferendis laborum corrupti odit minus magni!`,
+    },
+];
+
 const ProductsPage = () => {
     return (
         <div className="flex justify-center py-10">
-            <CardProduct>
-                <CardProduct.Header image="/img/2.jpg" />
-                <CardProduct.Body title="Tea">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur nulla corrupti, vitae quae quidem iusto modi maiores! Eligendi nihil repudiandae odio debitis consequatur est ullam, odit placeat similique ducimus!</CardProduct.Body>
-                <CardProduct.Footer price="Rp 21.000,00" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image="/img/1.jpg" />
-                <CardProduct.Body title="Coffee">Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque consectetur nulla corrupti, vitae quae quidem iusto modi maiores! Eligendi nihil repudiandae odio debitis consequatur est ullam, odit placeat similique ducimus!</CardProduct.Body>
-                <CardProduct.Footer price="Rp 23.000,00" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>{product.description}</CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))}
         </div>
     );
 };
